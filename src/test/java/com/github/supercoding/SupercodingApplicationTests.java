06d203b1c82c100be85ea0053ec35ab492b534fd
Kyoungwon Min <mkkw2047@gmail.com>
2023-08-01 14:09:22 +0900

    Initial commit

---
+++ src/test/java/com/github/supercoding/SupercodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.supercoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
